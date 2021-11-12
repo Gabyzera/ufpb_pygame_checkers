f34e9a1b6627ff4334721beb2c1dab4f69cfba77
RBzada <94180312+RBzada@users.noreply.github.com>
2021-11-12 10:44:35 -0300

    teste

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import pygame
+import pygame 
+
+pygame.init()
+
+largura = 600
+altura = 600
+
+tela = pygame.display.set_mode((largura, altura))
+nome = pygame.display.set_caption('Jogo de xadrez')
+
+preto = (0, 0, 0)
+branco = (255, 255, 255)
+
+tamanho = pygame.Rect(0, 0, 75, 75)
+
+pygame.draw.rect(tela, (branco), tamanho)
