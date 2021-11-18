4e8c3ce1d0d795020e7cb791496b8b4e09b54e58
Ismael M Araujo <ds.ismael.araujo2003@gmail.com>
2021-11-18 20:45:27 -0300

    tabuleiro versão 2

--- main.py
+++ main.py
@@ -8,45 +8,22 @@ branco = (255, 255, 255)
 tamanho = 75
 linhas_colunas = 8
 
-tela = pygame.display.set_mode((linhas_colunas * tamanho, (linhas_colunas+1)*tamanho))
+tela = pygame.display.set_mode(((linhas_colunas+2) * tamanho, (linhas_colunas+2)*tamanho))
 nome = pygame.display.set_caption('Xadrez')
-tela.fill(preto)
-pygame.draw.rect(tela, branco, (0,0,75,75),0)
-pygame.draw.rect(tela, branco, (150,0,75,75),0)
-pygame.draw.rect(tela, branco, (300,0,75,75),0)
-pygame.draw.rect(tela, branco, (450,0,75,75),0)
-pygame.draw.rect(tela, branco, (600,0,75,75),0)
-pygame.draw.rect(tela, branco, (75,75,75,75),0)
-pygame.draw.rect(tela, branco, (225,75,75,75),0)
-pygame.draw.rect(tela, branco, (375,75,75,75),0)
-pygame.draw.rect(tela, branco, (525,75,75,75),0)
-pygame.draw.rect(tela, branco, (0,150,75,75),0)
-pygame.draw.rect(tela, branco, (150,150,75,75),0)
-pygame.draw.rect(tela, branco, (300,150,75,75),0)
-pygame.draw.rect(tela, branco, (450,150,75,75),0)
-pygame.draw.rect(tela, branco, (600,150,75,75),0)
-pygame.draw.rect(tela, branco, (75,225,75,75),0)
-pygame.draw.rect(tela, branco, (225,225,75,75),0)
-pygame.draw.rect(tela, branco, (375,225,75,75),0)
-pygame.draw.rect(tela, branco, (525,225,75,75),0)
-pygame.draw.rect(tela, branco, (0,300,75,75),0)
-pygame.draw.rect(tela, branco, (150,300,75,75),0)
-pygame.draw.rect(tela, branco, (300,300,75,75),0)
-pygame.draw.rect(tela, branco, (450,300,75,75),0)
-pygame.draw.rect(tela, branco, (600,300,75,75),0)
-pygame.draw.rect(tela, branco, (75,375,75,75),0)
-pygame.draw.rect(tela, branco, (225,375,75,75),0)
-pygame.draw.rect(tela, branco, (375,375,75,75),0)
-pygame.draw.rect(tela, branco, (525,375,75,75),0)
-pygame.draw.rect(tela, branco, (0,450,75,75),0)
-pygame.draw.rect(tela, branco, (150,450,75,75),0)
-pygame.draw.rect(tela, branco, (300,450,75,75),0)
-pygame.draw.rect(tela, branco, (450,450,75,75),0)
-pygame.draw.rect(tela, branco, (600,450,75,75),0)
-pygame.draw.rect(tela, branco, (75,525,75,75),0)
-pygame.draw.rect(tela, branco, (225,525,75,75),0)
-pygame.draw.rect(tela, branco, (375,525,75,75),0)
-pygame.draw.rect(tela, branco, (525,525,75,75),0)
+
+tela.fill(branco)
+
+count = 0
+for i in range(1,linhas_colunas+1):
+    for z in range(1,linhas_colunas+1):
+        if count % 2 == 0:
+            pygame.draw.rect(tela, branco,[tamanho*z,tamanho*i,tamanho,tamanho])
+        else:
+            pygame.draw.rect(tela, preto, [tamanho*z,tamanho*i,tamanho,tamanho])
+        count +=1
+    count-=1
+
+pygame.draw.rect(tela,preto,[tamanho,tamanho,linhas_colunas*tamanho,linhas_colunas*tamanho],1)
 
 pygame.display.update()
 
