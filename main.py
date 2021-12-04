757b1acd4a6344dcda43dbd0858a825cff8984aa
Ismael M Araujo <ds.ismael.araujo2003@gmail.com>
2021-12-04 03:50:53 -0300

    atualizando tela e modo de jogo

--- main.py
+++ main.py
@@ -14,17 +14,26 @@ linhas_colunas = 8
 tela = pygame.display.set_mode(((linhas_colunas + 2) * tamanho, (linhas_colunas + 2) * tamanho))
 nome = pygame.display.set_caption('Damas | Chekers')
 
-# adicionando sprites:
-dama_clara = pygame.transform.scale(pygame.image.load("assets/sprites/dama_v2.png"), (tamanho - 2, tamanho - 2))
-dama_escura = pygame.transform.scale(pygame.image.load("assets/sprites/dama_v1.png"), (tamanho - 2, tamanho - 2))
-rainha_clara = pygame.transform.scale(pygame.image.load("assets/sprites/rainha_v2.png"), (tamanho - 2, tamanho - 2))
-rainha_escura = pygame.transform.scale(pygame.image.load("assets/sprites/rainha_v1.png"), (tamanho - 2, tamanho - 2))
-
-
 # Funcoes
-def Start():
+def Start(modo):
     tela.fill(preto)
     tela.blit(pygame.font.SysFont('Comic Sans MS', 30).render('RECOMEÇAR', False, azul), (250, 15))
+    tela.blit(pygame.font.SysFont('Comic Sans MS', 30).render('UFPB', False, bege), (550, 15))
+    tela.blit(pygame.transform.scale(pygame.image.load("assets/sprites/ufpb.png"), (tamanho, tamanho)), (tamanho * 9, tamanho * 0))
+
+    # adicionando sprites:
+    if modo == 1:
+        tela.blit(pygame.font.SysFont('Comic Sans MS', 30).render('3° IDADE', False, azul), (0, 15))
+        dama_clara = pygame.transform.scale(pygame.image.load("assets/sprites/dama_v4.png"), (tamanho - 2, tamanho - 2))
+        dama_escura = pygame.transform.scale(pygame.image.load("assets/sprites/dama_v3.png"), (tamanho - 2, tamanho - 2))
+        rainha_clara = pygame.transform.scale(pygame.image.load("assets/sprites/rainha_v4.png"), (tamanho - 2, tamanho - 2))
+        rainha_escura = pygame.transform.scale(pygame.image.load("assets/sprites/rainha_v3.png"), (tamanho - 2, tamanho - 2))
+    else:
+        tela.blit(pygame.font.SysFont('Comic Sans MS', 30).render('CLASSICO', False, azul), (0, 15))
+        dama_clara = pygame.transform.scale(pygame.image.load("assets/sprites/dama_v2.png"), (tamanho - 2, tamanho - 2))
+        dama_escura = pygame.transform.scale(pygame.image.load("assets/sprites/dama_v1.png"), (tamanho - 2, tamanho - 2))
+        rainha_clara = pygame.transform.scale(pygame.image.load("assets/sprites/rainha_v2.png"), (tamanho - 2, tamanho - 2))
+        rainha_escura = pygame.transform.scale(pygame.image.load("assets/sprites/rainha_v1.png"), (tamanho - 2, tamanho - 2))
 
     count = 0
     for i in range(1, linhas_colunas + 1):
@@ -45,6 +54,10 @@ def Start():
         tela.blit(dama_escura, (tamanho * i + 1, tamanho * 2 + 1))
         tela.blit(dama_clara, (tamanho * i + 1, tamanho * 6 + 1))
         tela.blit(dama_clara, (tamanho * i + 1, tamanho * 8 + 1))
+    tela.blit(dama_clara, (tamanho * 0 + 1, tamanho * 4 + 1))
+    tela.blit(pygame.font.SysFont('Comic Sans MS', 30).render('0', False, bege), (30, 360))
+    tela.blit(dama_escura, (tamanho * 9 + 1, tamanho * 4 + 1))
+    tela.blit(pygame.font.SysFont('Comic Sans MS', 30).render('0', False, bege), (660, 360))
 
     pygame.display.update()
 
@@ -58,7 +71,7 @@ def Start():
                          'Pdama_preta9': (1, 2), 'Pdama_preta10': (1, 4), 'Pdama_preta11': (1, 6),
                          'Pdama_preta12': (1, 8)}
 
-    return posicoes_iniciais
+    return posicoes_iniciais, dama_clara, dama_escura, rainha_clara, rainha_escura
 
 
 def quadrado(square, border):  # Pinta quadrado limpo novamente:
@@ -75,6 +88,23 @@ def quadrado(square, border):  # Pinta quadrado limpo novamente:
         else:
             pygame.draw.rect(tela, bege, [tamanho * (square[1]), tamanho * (square[0]), tamanho, tamanho])
 
+
+def ponts(posicoes):
+    pont_claras = 0
+    pont_escuras = 0
+    claras = list(posicoes.values())[12:]
+    escuras = list(posicoes.values())[:12]
+    for p in claras:
+        if p == None:
+            pont_claras += 1
+    for p in escuras:
+        if p == None:
+            pont_escuras += 1
+
+    pygame.draw.rect(tela, preto, [tamanho * 9, tamanho * 5, tamanho, tamanho])
+    pygame.draw.rect(tela, preto, [tamanho * 0, tamanho * 5, tamanho, tamanho])
+    tela.blit(pygame.font.SysFont('Comic Sans MS', 30).render(str(pont_claras), False, bege), (30, 360))
+    tela.blit(pygame.font.SysFont('Comic Sans MS', 30).render(str(pont_escuras), False, bege), (660, 360))
 
 def select(pos, possibilidades):
     print('run')
@@ -278,14 +308,15 @@ def move(pos, Old_Pos, posicoes, turn, possibilidades, comida, comer):
 
     return posicoes, turn, possibilidades, Old_Pos ## trocar ultimo valor por 'pos' para resolver erro
 
-
-posicoes = Start()  # Iniciando o Jogo
+modo = -1
+posicoes,dama_clara, dama_escura, rainha_clara,rainha_escura = Start(modo)  # Iniciando o Jogo
 jogo = True
 selecao = ()  # tupla para armazenar selecao do jogador
 possibilidades = []
 turno = 1
 OldPos = 0
 comida = [0]
+comer = 0
 while jogo:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -297,11 +328,16 @@ while jogo:
             linha = location[1] // tamanho
             if selecao == (linha, coluna):
                 if selecao == (0, 3) or selecao == (0, 4) or selecao == (0, 5) or selecao == (0, 6):  # Recomeçar
-                    posicoes = Start()
+                    posicoes,dama_clara, dama_escura, rainha_clara,rainha_escura = Start(modo)
+                    turno = 1
+                if selecao == (0, 1) or selecao == (0, 0):
+                    modo = -modo
+                    posicoes,dama_clara, dama_escura, rainha_clara,rainha_escura = Start(modo)
                     turno = 1
                 else:  # Movendo
                     posicoes, turno, possibilidades, OldPos = move(selecao, OldPos, posicoes, turno, possibilidades, comida, comer)
                 selecao = ()  # tornar seleçao vazia
+                ponts(posicoes)
 
             else:
                 selecao = (linha, coluna)
